be89300104fae817f8d6b1a1a8047a4ce421f761
caryoscelus <caryoscelus@gmx.com>
2014-02-04 16:20:15 +0400

    ..

--- src/Main.hs
+++ src/Main.hs
@@ -2,9 +2,9 @@
 -- {-# LANGUAGE FlexibleContexts #-}
 
 import Control.Monad
-import Control.Applicative
+-- import Control.Applicative
 import Data.Maybe
-import Control.Monad.Writer
+-- import Control.Monad.Writer
 import Data.Dynamic
 
 import Prelude hiding ((.), id, filter, null)
@@ -13,7 +13,7 @@ import qualified Prelude as Prelude
 import FRP.Netwire hiding (empty, unless)
 import Control.Wire hiding (empty, unless)
 
-import Linear.V2
+-- import Linear.V2
 
 import Game.Graphics
 
@@ -26,10 +26,6 @@ import World
 
 -- import Game.Graphics.Utils
 -- import Graphics.Rendering.OpenGL.Raw.Core32
-
-windowWidth, windowHeight :: Num a => a
-windowWidth = 800
-windowHeight = 600
 
 initGL :: IO (GLFW.Window, GraphicsState)
 initGL = do
@@ -51,52 +47,35 @@ renderFrame (window, graphics) frame = do
     _ <- draw graphics frame
     GLFW.swapBuffers window
 
-getSprite :: IO (Space Sprite)
-getSprite = do
-    etex <- loadTexture Standard Linear "16x16-red.png"
-    let tex = either error id etex
-    return $ sprite (V2 0 0) (V2 8 8) tex
-
-instance Monoid (Space a) where
-    mempty = empty
-    mappend = (<|>)
+-- instance Monoid (Space a) where
+--     mempty = empty
+--     mappend = (<|>)
 
 -- type RenderQueue = Writer [Space Sprite] ()
-type RenderQueue = Writer (Space Sprite) ()
+-- type RenderQueue = Writer (Space Sprite) ()
 
-doDraw :: Space Sprite -> RenderQueue
-doDraw = tell -- . (:[])
+-- doDraw :: Space Sprite -> RenderQueue
+-- doDraw = tell -- . (:[])
 
-v2Cast :: (Real a, Fractional b) => V2 a -> V2 b
-v2Cast (V2 x y) = V2 (realToFrac x) (realToFrac y)
+-- v2Cast :: (Real a, Fractional b) => V2 a -> V2 b
+-- v2Cast (V2 x y) = V2 (realToFrac x) (realToFrac y)
 
-drawAt :: (Real a) => Space Sprite -> V2 a -> RenderQueue
-drawAt spr xy = doDraw ((translate (v2Cast xy)) *> spr)
+-- drawAt :: (Real a) => Space Sprite -> V2 a -> RenderQueue
+-- drawAt spr xy = doDraw ((translate (v2Cast xy)) *> spr)
 
-frame2_render :: Space Sprite -> RenderQueue
-frame2_render spr = do
-    drawAt spr (V2 4 4)
-    drawAt spr (V2 44 44)
+-- fixCoords :: Space ()
+-- fixCoords = do
+--     -- scale coords from (-1, 1) to (-size/2, size/2)
+--     scale $ V2 (recip windowWidth * 2) (0 - recip windowHeight * 2)
+--     -- translate coords to (0, size)
+--     translate $ V2 (-windowWidth / 2) (-windowHeight / 2)
 
-fixCoords :: Space ()
-fixCoords = do
-    -- scale coords from (-1, 1) to (-size/2, size/2)
-    scale $ V2 (recip windowWidth * 2) (0 - recip windowHeight * 2)
-    -- translate coords to (0, size)
-    translate $ V2 (-windowWidth / 2) (-windowHeight / 2)
-
-runRender :: RenderQueue -> Space Sprite
-runRender wr = do
-    fixCoords
-    let ((), sprs) = runWriter wr
---     msum sprs
-    sprs
-
-frame2 :: Space Sprite -> Space Sprite
-frame2 spr = runRender $ frame2_render spr
-
-frame :: Space Sprite -> Space Sprite
-frame = frame2
+-- runRender :: RenderQueue -> Space Sprite
+-- runRender wr = do
+--     fixCoords
+--     let ((), sprs) = runWriter wr
+-- --     msum sprs
+--     sprs
 
 gFail :: (Typeable e) => e -> a
 gFail x = error $ fromDyn (toDyn x) ("Unknown error produced by " ++ show (typeOf x))
@@ -112,11 +91,6 @@ glGo inputState textures screen s w = do
 main :: IO ()
 main = do
     (window, graphics) <- initGL
-    sprite0 <- getSprite
-    let f = frame sprite0
     
     texs <- (load :: IO SnakeTextures)
---     let texs' :: SnakeTextures
---         texs' = texs
     glGo () texs (window, graphics) clockSession_ $ snake (snakeWorld 10 10)
-    renderFrame (window, graphics) f
--- src/Render.hs
+++ src/Render.hs
@@ -2,11 +2,17 @@
 
 module Render where
 
+import Control.Applicative
+
 import Linear.V2
 
 import Game.Graphics
 
 import World
+
+windowWidth, windowHeight :: Num a => a
+windowWidth = 800
+windowHeight = 600
 
 type Image = Space Sprite
 type Rendered = Image
@@ -16,6 +22,13 @@ class Loadable t where
 
 class (Loadable t) => Renderable r t where
     render :: t -> r -> Rendered
+
+fixCoords :: Space ()
+fixCoords = do
+    -- scale coords from (-1, 1) to (-size/2, size/2)
+    scale $ V2 (recip windowWidth * 2) (0 - recip windowHeight * 2)
+    -- translate coords to (0, size)
+    translate $ V2 (-windowWidth / 2) (-windowHeight / 2)
 
 -- snake
 data SnakeTextures = SnakeTextures
@@ -29,4 +42,4 @@ instance Loadable SnakeTextures where
         return . SnakeTextures $ sprite (V2 0 0) (V2 8 8) tex
 
 instance Renderable SnakeWorld SnakeTextures where
-    render texs world = getRedSquare texs
+    render texs world = fixCoords *> getRedSquare texs
