887aeb6dd0940f1fc02d54612e54a78ddc1868d9
caryoscelus <caryoscelus@gmx.com>
2014-02-07 13:22:18 +0400

    inputHandler introduction

--- src/Game.hs
+++ src/Game.hs
@@ -11,6 +11,8 @@ import Control.Wire hiding (empty)
 
 import System.Random (StdGen)
 
+import Graphics.UI.GLFW (Key(..), KeyState(..))
+
 import World
 
 data GameStatus = Playing | Paused | Fail | Win | NotStarted deriving (Show)
@@ -29,3 +31,20 @@ game :: (MonadFix m, Monoid e, HasTime t s, Fractional t)
      => Int -> Int -> StdGen -> Wire s e m InputState GameState
 game xs ys gen = (unless getEsc >>> mkConst (Right stateNotStarted))
              --> (snake (snakeWorld xs ys gen) >>^ statePlaying)
+
+
+noInput :: InputState
+noInput = InputState False False False False False
+
+makeInput :: Key -> InputState
+makeInput k = case k of
+                Key'Up     -> noInput { getUp    = True }
+                Key'Down   -> noInput { getDown  = True }
+                Key'Right  -> noInput { getRight = True }
+                Key'Left   -> noInput { getLeft  = True }
+                Key'Escape -> noInput { getEsc   = True }
+                _          -> noInput
+
+inputHandler :: (Monad m, Monoid e) => Wire s e m (Event Key) InputState
+inputHandler = (hold >>^ makeInput)
+           <|> arr (const noInput)
