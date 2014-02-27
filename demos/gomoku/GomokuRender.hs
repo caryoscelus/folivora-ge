a5fa1d2df6840be8dd243067eab1d6eedf4bccd1
caryoscelus <caryoscelus@gmx.com>
2014-02-27 17:04:56 +0400

    building gomoku

--- demos/gomoku/GomokuRender.hs
+++ demos/gomoku/GomokuRender.hs
@@ -18,13 +18,31 @@ import World
 import Game
 
 data GomokuTextures = GomokuTextures
+        { getRedSquare :: Image
+        , getGreenSquare :: Image
+        , getNormalFont :: Font
+        }
 
 instance Loadable GomokuTextures where
     load = do
+        etex <- loadTexture Standard Linear "tiles-16x16.png"
+        let tex = either error id etex
+        font <- loadFont "LiberationMono-Regular.ttf"
         return GomokuTextures
+                { getRedSquare   = sprite (V2 0 0) (V2 16 16) tex
+                , getGreenSquare = sprite (V2 16 0) (V2 16 16) tex
+                , getNormalFont  = font
+                }
 
 instance Renderable GomokuWorld GomokuTextures where
-    render texs world = empty
+    render texs world = translate (V2 16 16)
+                     *> renderTable tr (getTC . getTable $ world)
+        where
+            tr (Just Player)   = getGreenSquare texs
+            tr (Just Computer) = getRedSquare texs
+            tr Nothing         = empty
 
 instance Renderable Game GomokuTextures where
-    render texs game = empty
+    render texs game = case getData game of
+        (Just world) -> render texs world
+        Nothing      -> empty
--- demos/gomoku/World.hs
+++ demos/gomoku/World.hs
@@ -1,3 +1,13 @@
 module World where
 
+import Game.Folivora.TileGrid
+
+data GomokuUser = Player | Computer deriving (Show, Eq)
+type GomokuTile = Maybe GomokuUser
+
+type GomokuTable = DefaultTileGrid GomokuTile
+
 data GomokuWorld = GomokuWorld
+        { getTable :: GomokuTable
+        , getTurn :: GomokuUser
+        } deriving (Show)
