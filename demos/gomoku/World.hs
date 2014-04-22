ce4189b176cba8466c2144b183d81932f76a9d34
caryoscelus <caryoscelus@gmx.com>
2014-04-22 22:39:29 +0400

    gomoku: check for winning after occupying tile

--- demos/gomoku/World.hs
+++ demos/gomoku/World.hs
@@ -5,6 +5,8 @@ module World where
 
 import Data.Monoid
 import Data.Maybe
+import Control.Arrow
+import Control.Applicative
 
 import Linear.V2
 
@@ -23,14 +25,17 @@ type GomokuTile = Maybe GomokuUser
 
 type GomokuTable = DefaultTileGrid GomokuTile
 
+data GomokuStatus = Playing | Win | Full deriving (Show, Eq)
+
 data GomokuWorld = GomokuWorld
         { getTable :: GomokuTable
         , getTurn :: GomokuUser
         , getPosition :: V2 Int
+        , getStatus :: GomokuStatus
         } deriving (Show)
 
 newWorld :: GomokuWorld
-newWorld = GomokuWorld (emptyGrid (V2 17 17)) Player1 (V2 5 5)
+newWorld = GomokuWorld (emptyGrid (V2 17 17)) Player1 (V2 5 5) Playing
 
 modifyPosition :: V2 Int -> GomokuWorld -> GomokuWorld
 modifyPosition diff w = w { getPosition = pos' }
@@ -43,14 +48,42 @@ modifyTable :: (GomokuTable -> GomokuTable) -> GomokuWorld -> GomokuWorld
 modifyTable f w = w { getTable = f (getTable w) }
 
 advanceTurn :: GomokuWorld -> GomokuWorld
-advanceTurn w@(GomokuWorld {..}) = w { getTurn = nextPlayer getTurn }
+advanceTurn w@(GomokuWorld {..}) =
+    if getStatus == Playing then
+        w { getTurn = nextPlayer getTurn }
+    else
+        w
+
+checkWin :: GomokuWorld -> GomokuWorld
+checkWin w =
+    if win then
+        w { getStatus = Win }
+    else
+        w
+    where
+        win = or $ (>=5) <$> ($w) <$> calcLen <$> [V2 0 1, V2 1 0, V2 1 1, V2 1 (-1)]
+        calcLen dir w = walkUntil (countId tile) dir w 0 + walkUntil (countId tile) (-dir) w 0 - 1
+        tile = getCell (getPosition w) (getTable w)
+        countId :: GomokuTile -> (GomokuWorld, Int) -> (Bool, Int)
+        countId tile0 (w, count) =
+            if getCell (getPosition w) (getTable w) == tile0 then
+                (True, count + 1)
+            else
+                (False, count)
+        walkUntil :: ((GomokuWorld, a) -> (Bool, a)) -> V2 Int -> GomokuWorld -> a -> a
+        walkUntil f dir w val =
+            if (fst $ f (w, val)) then
+                walkUntil f dir (modifyPosition dir w) (snd $ f (w, val))
+            else
+                val
 
 -- try to occupy current tile and change active user if success
 occupyTile :: GomokuWorld -> GomokuWorld
 occupyTile w@(GomokuWorld {..}) =
-    if tileEmpty then
-        advanceTurn
-      . modifyTable (setCell getPosition (Just getTurn))
+    if tileEmpty && getStatus == Playing then
+        modifyTable (setCell getPosition (Just getTurn))
+    >>> checkWin
+    >>> advanceTurn
       $ w
     else
         w
